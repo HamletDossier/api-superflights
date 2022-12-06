6abc2cab966e2ebd3b5375a7eb32030c4c0ff716
Hamlet <dossier@hamletideas.com>
2022-12-06 17:56:38 -0600

    Add CRUD flight

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PassengerModule } from './passenger/passenger.module';
+import { FlightModule } from './flight/flight.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
 	  envFilePath:['.env.development'],
 	  isGlobal: true
-  }),MongooseModule.forRoot(process.env.URI_MONGODB), UserModule, PassengerModule
+  }),MongooseModule.forRoot(process.env.URI_MONGODB), UserModule, PassengerModule, FlightModule
 ],
   controllers: [AppController],
   providers: [AppService],
--- src/common/models/models.ts
+++ src/common/models/models.ts
@@ -1,2 +1,3 @@
 export const USER = {name: 'users'}
 export const PASENGER = {name: 'passengers'}
+export const FLIGHT = { name: 'flights'}
